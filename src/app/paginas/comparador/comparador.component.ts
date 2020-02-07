42e91035d080d894f671cfd78339efc50d2992be
Curso <Curso@IPARTEKAULA.COM>
2020-02-07 13:58:58 +0100

    comparador frutas

--- src/app/paginas/comparador/comparador.component.ts
+++ src/app/paginas/comparador/comparador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FRUTAS } from 'src/app/frutas';
 
 @Component({
   selector: 'app-comparador',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComparadorComponent implements OnInit {
 
-  constructor() { }
+  frutas: Array<any>;
+  fruta1: any;
+  fruta2: any;
+
+  constructor() { 
+
+    console.trace('ComparadorComponent constructor');
+
+    this.frutas = FRUTAS;
+
+    this.fruta1 = this.frutas[0];
+    this.fruta2 = this.frutas[1];
+
+  } //fin constructor
+
 
   ngOnInit() {
+
+    console.trace('ComparadorComponent ngOnInit');
+
+  } //fin ngOnInit
+
+
+  seleccionarFruta( frutaSeleccionada: any) {
+    console.debug('click frutaSeleccionada', frutaSeleccionada);    
+    this.fruta2 = this.fruta1;
+    this.fruta1 = frutaSeleccionada;
   }
 
-}
+} //fin ComparadorComponent

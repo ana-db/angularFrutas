ad11abff9560340a0070ebf4edb522c4bb097068
Ana Diez <anadiezbravo@outlook.com>
2020-02-08 22:45:55 +0100

    postre según idFruta enviada por la url

--- src/app/frutas.ts
+++ src/app/frutas.ts
@@ -14,6 +14,20 @@ export const FRUTAS =
             "Potasio",
             "Sodio"
         ],
+        "nombrePostre":"Tarta de manzana",
+        "cocineroPostre":"Eva Arguiñano",
+        "fotoPostre": "https://vod-hogarmania.atresmedia.com/hogarmania/images/images01/2014/02/05/5bf2d9f5a649820001d3dd41/1239x697.jpg",
+        "ingredientesPostre": 
+        [
+            "Manzanas",
+            "Harina",
+            "Huevos",
+            "Levadura",
+            "Leche",
+            "Azucar",
+            "Mermelada de albaricoque"
+        ],
+        "recetaPostre": "1) Coloca la harina, la levadura, los huevos, el azúcar, 2 manzanas (peladas y cortadas en dados) y la leche en el vaso batidor. Tritura hasta que quede una masa homogénea. 2) Vierte la mezcla sobre el molde untado de mantequilla y espolvoreado de harina. Pela las otras 2 manzanas, córtalas en láminas y cubre la superficie de la tarta. 3) Hornéala a 180ºC durante 45 minutos (con el horno precalentado). 4) Deja que se temple un poco, desmolda y con ayuda de un pincel unta la superficie con un poco de mermelada de albaricoque. 5) Adorna la tarta de manzana con los frutos rojos y unas hojas de menta. Sirve la tarta de manzana.",
         "precio": "1.5"
     },
     {
@@ -32,6 +46,18 @@ export const FRUTAS =
             "Sodio",
             "Yodo"
         ],
+        "nombrePostre":"Plátanos flameados con salsa de cítricos",
+        "cocineroPostre":"Eva Arguiñano",
+        "fotoPostre": "https://www.hogarmania.com/archivos/201105/platanos-flambeados-con-salsa-de-citricos-xl-668x400x80xX.jpg",
+        "ingredientesPostre": 
+        [
+            "Plátanos",
+            "Frutas cítricas: naranja y limón",
+            "Mantequilla",
+            "Azúcar",
+            "Helado"
+        ],
+        "recetaPostre": "1) Pela la naranja y el limón, exprímelos (reserva sus zumos) y corta en juliana fina unas tiras de sus cáscaras. Para quitarles el amargor, escáldalas en un cazo con agua que las cubra. Al primer hervor, baja el fuego y cocina durante 5-10 minutos. Cuela. 2) Pela los plátanos, pártelos por la mitad a lo largo y fríelos en una sartén con la mantequilla. Añade el licor de naranja y el ron y flamea. Retira los plátanos y mantenlos calientes. En la misma sartén, vierte el zumo de limón y el zumo de naranja reservados, agrega el azúcar blanco y el azúcar moreno y deja reducir. Incorpora las cáscaras de los cítricos escaldadas y mezcla hasta que adquiera una textura de miel. 3) Sirve los plátanos con las cáscaras de los cítricos por encima y acompaña con unas bolas de helado (pueden ser una de menta y chocolate y otra solo de chocolate) en cada plato. Decora con menta y salsea con la salsa.",
         "precio": "3"
     },
     {
@@ -48,6 +74,16 @@ export const FRUTAS =
             "Magnesio",
             "Potasio"
         ],
+        "nombrePostre":"Mermelada de naranja casera",
+        "cocineroPostre":"Eva Arguiñano",
+        "fotoPostre": "https://www.hogarmania.com/archivos/201110/mermelada-naranja-668x400x80xX.jpg",
+        "ingredientesPostre": 
+        [
+            "Naranjas",
+            "Zumo de limón",
+            "Azúcar"
+        ],
+        "recetaPostre": "1) Lava y pela las naranjas. Quita toda la parte blanca, trocea la pulpa y quita las pepitas, si las tiene. Corta la piel en tiras finas y añádela a la pulpa. 2) Pon la pulpa y la piel a cocer en una cazuela durante 40 minutos a fuego lento, removiéndolo para que no se pegue. 3) Añade el azúcar y el zumo de limón y déjalo cocer 10 minutos más. 4) Deja enfriar y estará lista para comer, sobre una tosta de pan o como acompañamiento de platos de carne. 5) Si quieres envasarlo al vacío, rellena los botes de cristal poco a poco y esterilízalos al baño maría durante 30 minutos. Guárdalos en un lugar seco, fresco y alejado de la luz.",
         "precio": "1"
     },
     {
@@ -63,6 +99,21 @@ export const FRUTAS =
             "Magnesio",
             "Potasio"
         ],
+        "nombrePostre":"Piña asada",
+        "cocineroPostre":"Eva Arguiñano",
+        "fotoPostre": "https://www.hogarmania.com/archivos/201105/pina-asada-xl-668x400x80xX.jpg",
+        "ingredientesPostre": 
+        [
+            "Piña",
+            "Naranja",
+            "Azúcar",
+            "Nuez moscada",
+            "Cuchada de canela",
+            "Frambuesas",
+            "Grosellas",
+            "Yogur natural o miel"
+        ],
+        "recetaPostre": "1) Pela la piña, córtala en rodajas y retírales el troncho central. 2) Lava la naranja y ralla la cáscara sobre el azúcar (reserva una cucharada), ralla también la nuez moscada e incorpórala al azucar. Pon los clavos bajo un trapo y machácalos con el mazo, agrega el polvo obtenido al azúcar y la canela. Mezcla bien. 3) Coloca las rodajas de piña en una fuente horno. Espolvorea las rodajas de piña con el azúcar especiada. Vierte el vinagre de frambuesa en los huecos de las rodajas. Introduce en el horno a 220º C durante 15 minutos. 4) Vierte el caldo resultante a una sartén, añade la cucharada de azúcar reservada anteriormente, las 5 frambuesas y una hoja de menta. Aplástalas con una cuchara de palo. 5) Sirve las rodajas de piña, acompaña algunas con miel y otras con yogur batido. Decora con grosellas y hojas de menta.",
         "precio": "3.5"
     },
     {
@@ -77,7 +128,30 @@ export const FRUTAS =
             "Magnesio",
             "Potasio"
         ],
+        "nombrePostre":"Fresas con yogur",
+        "cocineroPostre":"Bruno Oteiza",
+        "fotoPostre": "https://www.hogarmania.com/archivos/201209/fresas-yogur-668x400x80xX.jpg",
+        "ingredientesPostre": 
+        [
+            "Fresas",
+            "Grosellas",
+            "Frambuesas",
+            "Yogur natural",
+            "Claras de huevo",
+            "Azúcar"
+        ],
+        "recetaPostre": "1) Para el sirope, lava las fresas, quítales el pedúnculo y trocéalas (reserva 2 enteras). Pon a reducir en una cazuela a fuego lento junto con las grosellas y el azúcar. Reserva. 2) Separa las claras del huevo, ponlas en un bol, añade una cucharadita de azúcar y bate con la batidora de montar hasta obtener merengue. Envuelve con el yogur suavemente y deja enfriar en el frigorífico. Reserva. 3) Lamina las fresas reservadas y colócalas en las paredes de los vasos (de cristal transparente). Coloca el sirope en el fondo y añade la mezcla de yogur y merengue por encima. En el último momento, reparte por encima las frambuesas y las grosellas, y decora con una hojita de menta.",
         "precio": "3"
     }
 
 ];
+
+
+/*
+"postre": 
+    [{
+        "nombre": "Tarta de manzana",
+        "foto": "https://images.unsplash.com/photo-1562007908-69cf18a6da04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "receta": "1) Coloca la harina, la levadura, los huevos, el azúcar, 2 manzanas (peladas y cortadas en dados) y la leche en el vaso batidor. Tritura hasta que quede una masa homogénea. 2) Vierte la mezcla sobre el molde untado de mantequilla y espolvoreado de harina. Pela las otras 2 manzanas, córtalas en láminas y cubre la superficie de la tarta. 3) Hornéala a 180ºC durante 45 minutos (con el horno precalentado). 4) Deja que se temple un poco, desmolda y con ayuda de un pincel unta la superficie con un poco de mermelada de albaricoque. 5) Adorna la tarta de manzana con los frutos rojos y unas hojas de menta. Sirve la tarta de manzana."
+    }]
+*/
--- src/app/paginas/postres/postres.component.ts
+++ src/app/paginas/postres/postres.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FRUTAS } from '../../frutas'
 
 @Component({
   selector: 'app-postres',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostresComponent implements OnInit {
 
-  constructor() { }
+  idFruta: number;
+  frutas : Array<any>;
+  frutaSeleccionada: Array<any>;
+
+  constructor(private route: ActivatedRoute) {
+
+    console.trace('PostresComponent constructor');
+
+    this.frutas = FRUTAS;
+
+    this.idFruta = 1;
+    this.route.params.subscribe( params => this.idFruta = params.pIdFruta ); //params devuelve un observable --> nos tenemos que subscribir
+   
+    this.frutaSeleccionada = this.frutas[this.idFruta-1]; 
+
+  } //fin constructor
 
   ngOnInit() {
-  }
+    console.trace('PostresComponent ngOnInit');
+  }//fin ngOnInit
 
-}
+} //fin PostresComponent

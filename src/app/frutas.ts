c440665f3c9cbcaa6dc51e97bf03068142deb67f
Ana Diez <anadiezbravo@outlook.com>
2020-02-08 13:52:09 +0100

    lista y detalle de las frutas

--- src/app/frutas.ts
+++ src/app/frutas.ts
@@ -5,7 +5,15 @@ export const FRUTAS =
         "nombre": "Manzana",
         "clasificacion": "Fruta dulce",
         "descripcion": "La manzana es el fruto ideal para tomar a cualquier hora y participa positivamente en la consecución del equilibrio alimentario. La piel puede ser de color verde, amarilla o rojiza, y la carne va desde un sabor agrio hasta el dulce. Es uno de los frutos más consumidos en el mundo. La gran cantidad de variedades existentes hacen que se encuentren al alcance del consumidor durante todo el año.",
-        "foto": "http://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/84.jpg",
+        "foto": "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "minerales": 
+        [
+            "Calcio",
+            "Fosforo",
+            "Magnesio",
+            "Potasio",
+            "Sodio"
+        ],
         "precio": "1.5"
     },
     {
@@ -13,7 +21,17 @@ export const FRUTAS =
         "nombre": "Platano",
         "clasificacion": "Exoticas",
         "descripcion": "El plátano o banano es una fruta amarilla, de forma alargada, que encontramos en el mercado en grupos de tres a veinte, de forma similar a un pepino triangular, oblongo y normalmente de color amarillo. Su sabor es más o menos dulce según la variedad.",
-        "foto": "http://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/1.jpg",
+        "foto": "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+        "minerales": 
+        [
+            "Potasio",
+            "Magnesio",
+            "Calcio",
+            "Cinc",
+            "Fosforo",
+            "Sodio",
+            "Yodo"
+        ],
         "precio": "3"
     },
     {
@@ -22,6 +40,14 @@ export const FRUTAS =
         "clasificacion": "Citricos",
         "descripcion": "La naranja es un fruto redondo, color naranja, consumido mayoritariamente en invierno. La pulpa del interior es también anaranjada y está formada por pequeñas bolsitas llenas de zumo. La naranja se usa para consumo en fresco y, para la industria, principalmente en zumo.",
         "foto": "http://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/22.jpg",
+        "minerales": 
+        [
+            "Calcio",
+            "Fosforo",
+            "Hierro",
+            "Magnesio",
+            "Potasio"
+        ],
         "precio": "1"
     },
     {
@@ -30,6 +56,13 @@ export const FRUTAS =
         "clasificacion": "Exoticas",
         "descripcion": "La piña madura tiene una fragancia muy singular. Es de hermoso color y agradable sabor agridulce. Se puede comer cruda o como ingrediente en zumos, conservas, licores, etc. Tanto el fruto como las hojas se usan en la preparación de compuestos medicinales.",
         "foto": "http://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/27.jpg",
+        "minerales": 
+        [
+            "Fosforo",
+            "Hierro",
+            "Magnesio",
+            "Potasio"
+        ],
         "precio": "3.5"
     },
     {
@@ -37,7 +70,13 @@ export const FRUTAS =
         "nombre": "Fresas",
         "clasificacion": "Bayas",
         "descripcion": "La fresa es un fruto de color rojo brillante, suculento y fragante que se obtiene de la planta que recibe su mismo nombre. En Occidente es considerada la reina de las frutas. Además de poderse comer cruda se puede consumir como compota, mermelada,... Es empleada con fines medicinales ya que posee excelentes propiedades que ayudan a preservar la salud.",
-        "foto": "http://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/95.jpg",
+        "foto": "https://images.unsplash.com/photo-1569613562636-7492d9f77aed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+        "minerales": 
+        [
+            "Fosforo",
+            "Magnesio",
+            "Potasio"
+        ],
         "precio": "3"
     }
 
--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FRUTAS } from '../../frutas'
 
 @Component({
   selector: 'app-inicio',
@@ -8,18 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class InicioComponent implements OnInit {
 
   title: string;
+  frutas : Array<any>;
+  frutaSeleccionada: Array<any>;
 
   constructor() { 
 
     console.trace('InicioComponent constructor');
 
-    this.title = 'Frutas';
+    this.title = 'Curiosidades sobre las frutas';
+    this.frutas = FRUTAS;
+    this.frutaSeleccionada = this.frutas[0]; 
 
   } //fin constructor
 
   
   ngOnInit() {
     console.trace('InicioComponent ngOnInit');
-  } //fin constructor
+  } //fin ngOnInit
+
+
+  //función para seleccionar una fruta y sacar su detalle: 
+  seleccionarFruta = function(fruta){
+    console.log('Hemos hecho click en la fruta %o', fruta ); 
+    this.frutaSeleccionada = fruta;
+  }
+
 
 } //InicioComponent
